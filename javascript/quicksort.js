66e10d268517bd0f9c52363e1675a52174072677
Aruna <aruna.evan15@gmail.com>
2022-03-08 16:25:09 -0800

    solution

--- javascript/quicksort.js
+++ javascript/quicksort.js
@@ -1,13 +1,31 @@
-function partition(array, low, high) {
-  // type your code in here
+function quicksort(array, low = 0, high = array.length-1) {
+  if (low >= high) return array;
+
+  let pIdx = partition(array, low, high);
+  quicksort(array, low, pIdx-1);
+  quicksort(array, pIdx+1, high);
+
+  return array;
 }
 
-function quicksort(array, low = 0, high = array.length - 1) {
-  // type your code here
+function partition(array, low, high) {
+  while (low <= high) {
+    let beforeHigh = high - 1;
+
+    if (array[low] > array[high]) {
+      [array[low], array[beforeHigh]] = [array[beforeHigh], array[low]];  
+      [array[beforeHigh], array[high]] = [array[high], array[beforeHigh]];
+      high--;
+    } else {
+      low ++;
+    }
+  }
+
+  return high;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+
   console.log("Expecting: [1, 2, 3, 4]");
   console.log(quicksort([3, 2, 1, 4]));
 
@@ -15,9 +33,56 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 2, 3, 4]");
   console.log(quicksort([1, 2, 2, 3, 4]));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log(quicksort([]));
+
+  console.log("");
+
+  console.log("Expecting: [1]");
+  console.log(quicksort([1]));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2, 3, 4]");
+  console.log(quicksort([4, 3, 2, 1]));
 }
 
 module.exports = quicksort;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase: implement quick sort
+ * 2. tests: (above)
+ * 3. pseudocode:
+ * 
+ * quicksort = recursively call on left and right arrays
+ * partition = returns final index of pivot element
+ * 
+ * 4. code: (above)
+ * 5. refactor:
+ * 6. optimize:
+ */
+
+
+// The below solution isn't "in-place", whereas quicksort should be!
+
+// function quicksort(array, low = 0, high = array.length - 1) {
+//   if (array.length <= 1) return array;
+
+//   let pivot = array[high];
+//   let leftArr = [];
+//   let rightArr = [];
+
+//   for (let i=low; i < array.length-1; i++) {
+//     if (array[i] < pivot) {
+//       leftArr.push(array[i]);
+//     } else {
+//       rightArr.push(array[i]);
+//     }
+//   }
+
+//   return [...quicksort(leftArr), pivot, ...quicksort(rightArr)]
+
+// }
